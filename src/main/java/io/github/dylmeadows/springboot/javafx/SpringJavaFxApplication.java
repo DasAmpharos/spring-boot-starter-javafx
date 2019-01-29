8cd56ddd39039b49f761a7c615cc890d34c0509d
Dylan Meadows <dylmeadows@gmail.com>
2019-01-29 08:15:50 -0500

    fixing spring configuration in build.gradle

--- src/main/java/io/github/dylmeadows/springboot/javafx/SpringJavaFxApplication.java
+++ src/main/java/io/github/dylmeadows/springboot/javafx/SpringJavaFxApplication.java
@@ -16,17 +16,25 @@ public abstract class SpringJavaFxApplication extends Application {
     private SpringFxmlLoader springFxmlLoader;
 
     @Override
-    public final void init() {
+    public final void init() throws Exception {
         String[] args = getParameters().getRaw().toArray(new String[0]);
         context = new SpringApplicationBuilder(getClass())
             .main(getClass())
             .headless(false)
             .run(args);
         springFxmlLoader = context.getBean(SpringFxmlLoader.class);
+        onInit();
     }
 
     @Override
-    public final void stop() {
+    public final void stop() throws Exception {
+        onStop();
         context.close();
+    }
+
+    protected void onInit() throws Exception {
+    }
+
+    protected void onStop() throws Exception {
     }
 }
