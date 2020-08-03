a6eae5601037d413bcd16210367e0733884b3c7c
Dylan Meadows <dylmeadows@gmail.com>
2020-08-03 09:31:36 -0400

    Changes from local

--- src/main/kotlin/io/github/dylmeadows/springboot/javafx/fxml/SpringFxmlLoader.kt
+++ src/main/kotlin/io/github/dylmeadows/springboot/javafx/fxml/SpringFxmlLoader.kt
@@ -5,8 +5,8 @@ import io.github.dylmeadows.commonkt.core.io.url
 import javafx.fxml.FXMLLoader
 import javafx.scene.Parent
 import javafx.util.Callback
+import org.springframework.beans.factory.BeanFactory
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
 import java.io.InputStream
 import java.net.URL
@@ -14,42 +14,56 @@ import java.util.*
 
 @Component
 class SpringFxmlLoader @Autowired constructor(
-    private val ctx: ApplicationContext) {
-
-    fun <T : Parent> load(resource: Resource): T {
+    private val beans: BeanFactory
+) {
+    fun <T : Parent> load(
+        resource: Resource
+    ): T {
         return load(resource.url!!)
     }
 
-    fun <T : Parent> load(resource: Resource, resources: ResourceBundle): T {
+    fun <T : Parent> load(
+        resource: Resource,
+        resources: ResourceBundle
+    ): T {
         return load(resource.url!!, resources)
     }
 
-    fun <T : Parent> load(location: URL): T {
+    fun <T : Parent> load(
+        location: URL
+    ): T {
         val loader = FXMLLoader()
         loader.location = location
-        loader.controllerFactory = Callback { ctx.getBean(it) }
+        loader.controllerFactory = Callback { beans.getBean(it) }
         return loader.load()
     }
 
-    fun <T : Parent> load(location: URL, resources: ResourceBundle): T {
+    fun <T : Parent> load(
+        location: URL,
+        resources: ResourceBundle
+    ): T {
         val loader = FXMLLoader()
         loader.location = location
         loader.resources = resources
-        loader.builderFactory
-        loader.controllerFactory = Callback { ctx.getBean(it) }
+        loader.controllerFactory = Callback { beans.getBean(it) }
         return loader.load()
     }
 
-    fun <T : Parent> load(inputStream: InputStream): T {
+    fun <T : Parent> load(
+        inputStream: InputStream
+    ): T {
         val loader = FXMLLoader()
-        loader.controllerFactory = Callback { ctx.getBean(it) }
+        loader.controllerFactory = Callback { beans.getBean(it) }
         return loader.load(inputStream)
     }
 
-    fun <T : Parent> load(inputStream: InputStream, resources: ResourceBundle): T {
+    fun <T : Parent> load(
+        inputStream: InputStream,
+        resources: ResourceBundle
+    ): T {
         val loader = FXMLLoader()
         loader.resources = resources
-        loader.controllerFactory = Callback { ctx.getBean(it) }
+        loader.controllerFactory = Callback { beans.getBean(it) }
         return loader.load(inputStream)
     }
 }
